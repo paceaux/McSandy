76e43dc7eecf184e0d85653211244944b1db6075
Frank Taylor <paceaux@madebypaceaux.com>
2014-02-13 18:13:01 -0600

    minor UI tweaks, added keystrokes, updated readme

--- README.md
+++ README.md
@@ -1,24 +1,46 @@
 # McSandy: The HTML5 offline Sandbox #
 
-A SFA (Single File Application ) that acts as your HTML, CSS, and JavaScript editor, and can work without an internet connection. 
+A SFA (Single File Application ) that acts as your HTML, CSS, and JavaScript editor;
 
  * Version: 0.2.0
  * License: Copyright 2014 Frank M. Taylor. All Rights Reserved
+ * Prerequisites: IE10+, other modern browsers
 
-## Version History ##
-This is a complete overhaul of McSandy 1.0. This version has **no external dependencies** It uses vanilla JavaScript. 
+## FAQs ##
+###What's an SFA? ###
 
+An SFA is a Single File Application. This means the entire application functions in a single file - no installation, extra files, or internet, is required to use it. Once you've downloaded McSandy (index.html), that's all you need. At 350 lines, that's not too bad, right?
+
+### What can I do with McSandy? ###
+McSandy lets you edit HTML, CSS and JavaScript with a live preview. Every time you leave the HTML, CSS, or JavaScript fields (with the tab button), live preview will be updated. You'll have a live-reload with the CSS. 
+
+### Can I save my work? ###
+Of course you can! McSandy uses `localstorage` to save your projects. You can save and delete any of your projects
+
+###Can I export my work? ###
+Not yet, but that is an upcoming feature. You will be able to export 
   
+### What are the technologies that McSandy uses? ###
+McSandy is an HTML5 application. It's using Vanilla JavaScript, but it makes use of three API's in particular
+ + localStorage (for storing data)
+ + `online` (for showing whether you have internet)
+ + Blob URLs (for doing the live-preview in an iframe)
+
+McSandy also uses the Flexbox module for its layout. Flexbox is supported in IE10+ 
 
 
-## Recently done
+## Recently done ##
  + got the flex layout working properly, header, main area, and footer distributely evenly
  + can now load projects from local storage
  + loading projects from LS puts the content in the editors
  + can delete projects
-##Upcoming To-dos
+ + changed orientation of the editor buttons
+ + can now save with `ctrl + s`
+ + live updating when editing the CSS
+ + corrected font in editor boxes
+
+## Upcoming To-dos ##
  + fix the delete button CSS. right now it's always *not* showing. we only want it to show when a project has been loaded
- + correct the font in the editor boxes
  + get some animation when you expand/collapse sections. 
 
 
--- mcsandy.js
+++ mcsandy.js
@@ -1,37 +1,4 @@
-var mcsandyUI;
-mcsandyUI = {
-    init: function () {
-        var _this = mcsandyUI;
-        _this.bindUiEvents();
-    },
-    data: {
-        onlineState: 'online',
-        onlineCtrl: document.getElementById('js-onlineStatus')
-    },
-    helpers: {
-    },
-    bindUiEvents: function () {
-        var _this = mcsandyUI;
-        window.addEventListener('load', function (e) {
-            _this.functions.handleConnection();
-        });
-        window.addEventListener("offline", _this.functions.handleConnection )
-        window.addEventListener("online", _this.functions.handleConnection )
-    },
-    functions: {
-        handleConnection: function (e) {
-            var _this = mcsandyUI,
-                ctrl = document.getElementById('js-onlineStatus');
-            _this.data.onlineState = navigator.onLine ? "online" : "offline";
-            if (_this.data.onlineState == "online"){
-                ctrl.className = ctrl.className.replace( /(?:^|\s)offline(?!\S)/g, " online");
-            } else {
-                ctrl.className = ctrl.className.replace( /(?:^|\s)online(?!\S)/g," offline");
-            }
-        }
-    }
-}
-mcsandyUI.init();
+//store is the local/session storage helper. 
 var store;
 store = {
     types: [localStorage,sessionStorage],
@@ -60,7 +27,106 @@ store = {
     }
 };
 
+//McSandy UI is the editor and its controls. It's the content that goes in
+var mcsandyUI;
+mcsandyUI = {
+    init: function () {
+        var _this = mcsandyUI;
+        _this.bindUiEvents();
+    },
+    data: {
+        onlineState: 'online',
+        onlineCtrl: document.getElementById('js-onlineStatus'),
+        els: {
+            projecSelect: document.getElementById('js-selectProjects')
+        },
+        keyMaps:{
+            save: {
+                83:false,
+                17:false
+            }
+        }
+    },
+    helpers: {
+        keyDown: function(e) {
+            var _this = mcsandyUI,
+                keyMaps = _this.data.keyMaps, 
+                saveMap = keyMaps.save;
 
+            //SAVE
+            if (e.keyCode in saveMap){
+                saveMap[e.keyCode] = true;
+                if(saveMap[17] && saveMap[83]) {
+                    mcsandy.functions.saveContent(e);
+                }
+            }
+        },
+        keyUp: function (e) {
+            var _this = mcsandyUI,
+                keyMaps = _this.data.keyMaps,
+                saveMap = keyMaps.save;
+                if (e.keyCode in keyMaps.save) {
+                    keyMaps.save[e.keyCode] = false;
+                }
+        }
+    },
+    bindUiEvents: function () {
+        var _this = mcsandyUI,
+            helpers = _this.helpers,
+            els = _this.data.els;
+
+        //CHECK FOR INTERNET CONNECTION
+        window.addEventListener('load', function (e) {
+            _this.functions.handleConnection();
+        });
+        window.addEventListener("offline", _this.functions.handleConnection );
+        window.addEventListener("online", _this.functions.handleConnection );
+
+        //SELECT A PROJECT
+        els.projecSelect.addEventListener('change', _this.functions.handleProjectSelect);
+
+        //KEYBOARD SHORTCUTS
+        document.addEventListener('keydown', helpers.keyDown)
+        document.addEventListener('keyup', helpers.keyUp)
+
+    },
+    functions: {
+        handleConnection: function (e) {
+            var _this = mcsandyUI,
+                ctrl = document.getElementById('js-onlineStatus');
+            _this.data.onlineState = navigator.onLine ? "online" : "offline";
+            if (_this.data.onlineState == "online"){
+                ctrl.className = ctrl.className.replace( /(?:^|\s)offline(?!\S)/g, " online");
+            } else {
+                ctrl.className = ctrl.className.replace( /(?:^|\s)online(?!\S)/g," offline");
+            }
+        },
+        handleProjectSelect: function (e) {
+            var _this = mcsandyUI,
+                proj = e.target.value,
+                projData = store.get(0,proj);
+            mcsandy.functions.updateContent(projData); // this is in the McSandy interface
+            _this.functions.updateEditors(projData.rawParts.html, projData.rawParts.css, projData.rawParts.js);
+            _this.functions.updateProjectName(projData.project);
+        },
+        updateEditors: function (html, css, js) {
+            var _this = mcsandyUI, 
+                ctrls = mcsandy.data.ctrls;
+            ctrls.html.value = html;
+            ctrls.css.value = css;
+            ctrls.js.value = js;
+        },
+        updateProjectName: function (projectName) {
+            var _this = mcsandyUI,
+                projectField = mcsandy.data.ctrls.project;
+            projectField.value = projectName;
+            projectField.placeholder = projectName;
+        }
+    }
+}
+mcsandyUI.init();
+
+//mcsandy stores the data and retrieves the data. it also prepares the iframe
     var mcsandy;
     mcsandy = {
         init: function () {
@@ -71,6 +137,7 @@ store = {
         data: {
             ctrls: {
                 projectSave: document.getElementById('js-projectSave'),
+                projectDel: document.getElementById('js-projectDel'),
                 project: document.getElementById('js-projectName'),
                 html: document.getElementById('js-html'),
                 css: document.getElementById('js-css'),
@@ -98,7 +165,15 @@ store = {
             prepareJS: function (js) {
                 return '<script type="text\/javascript">' + js + '<\/script>'
             },
-            prepareBlobParts: function () {
+            createRawParts:function (html, css, js) {
+                var rawParts = {
+                    html: html,
+                    css: css,
+                    js: js
+                }
+                return rawParts;
+            },
+            wrapProjectParts: function (html, css, js) {
                 var _this = mcsandy,
                     helpers = _this.helpers,
                     blobData = _this.blobData,
@@ -106,11 +181,18 @@ store = {
                     html = helpers.prepareHTML(ctrls.html.value),
                     reset = helpers.prepareCSS(blobData.reset),
                     jquery = blobData.jquery,
-                    css = helpers.prepareCSS(ctrls.css.value),
+                    css =  helpers.prepareCSS(ctrls.css.value),
                     head = '<head>'+css+'</head>',
                     js = helpers.prepareJS(ctrls.js.value),
-                    blobKit = [head,html,js,jquery];
+                    blobKit = [head,html,jquery,js];
                 return blobKit;
+            },
+            createProject: function (projectName, rawParts, blobArray) {
+                return {
+                    'project': projectName,
+                    blobArray: blobArray,
+                    rawParts: rawParts
+                } 
             },
             prepareResult: function (parts) {
                 var _this = mcsandy,
@@ -142,40 +224,57 @@ store = {
             var _this = mcsandy,
                 functions = _this.functions,
                 ctrls = _this.data.ctrls;
-            ctrls.css.addEventListener('change',functions.updateContent);
-            ctrls.html.addEventListener('change',functions.updateContent);
-            ctrls.js.addEventListener('change',functions.updateContent);
-            ctrls.projectSave.addEventListener('click', functions.saveContent)
+            ctrls.css.addEventListener('change',function (e) {
+                functions.updateContent();
+            });
+            ctrls.css.addEventListener('keyup',function (e) {
+                functions.updateContent();
+            });
+
+            ctrls.html.addEventListener('change',function (e) {
+                functions.updateContent();
+            });
+            ctrls.js.addEventListener('change',function (e) {
+                functions.updateContent();
+            });
+            ctrls.projectSave.addEventListener('click', functions.saveContent);
+            ctrls.projectDel.addEventListener('click', functions.delContent);
         },
         functions: {
             getProjects: function () {
                 var _this = mcsandy,
                     projects = _this.helpers.getStoredProjects(),
                     select = document.getElementById('js-selectProjects');
-                console.log(projects);
                 projects.forEach(function(el) {
                     var option = _this.helpers.createSelectOption(el);
                     select.appendChild(option);
                 })
             },
-
-            updateContent: function (e) {
-                var _this = mcsandy,
+            updateContent: function (loadedParts) {
+                 var _this = mcsandy,
                     iframe = _this.data.targets.iframe,
-                    parts = _this.helpers.prepareBlobParts(),
-                    result = _this.helpers.prepareResult(parts);
+                    parts =  loadedParts !== undefined ? loadedParts.blobArray : _this.helpers.wrapProjectParts();
+                   var result = _this.helpers.prepareResult(parts);
                 iframe.src = window.URL.createObjectURL(result);
+            },
+            delContent: function (e) {
+                e.preventDefault();
+                var _this = mcsandy,
+                    projectName = _this.data.ctrls.project.value;
+                store.del(0,projectName);
+                _this.functions.getProjects();
             },
             saveContent: function (e) {
                 e.preventDefault();
                 var _this = mcsandy,
-                    parts = _this.helpers.prepareBlobParts(),
+                    ctrls = _this.data.ctrls,
+                    rawParts = _this.helpers.createRawParts(ctrls.html.value, ctrls.css.value, ctrls.js.value),
+                    blobArray = _this.helpers.wrapProjectParts(),
                     projectName = _this.data.ctrls.project.value,
-                    project = {
-                        'project': projectName,
-                        parts: parts
-                    };
+                    project = _this.helpers.createProject(projectName, rawParts, blobArray)
+                   
                 store.set(0, projectName, project);
+                _this.functions.getProjects();
             }
         }
     }
