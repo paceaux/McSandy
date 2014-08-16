4d7ad55744b89cf86126bc290d8f2e9db617dc10
Frank Taylor <paceaux@paceaux.local>
2014-08-16 16:05:18 +0200

    error in the store.js file. when getting an item as a member of an array, function was returning the string value of the key, instead of the value

--- preAssets/js/store.js
+++ preAssets/js/store.js
@@ -4,23 +4,25 @@ store = {
         return typeof v !== "object" ? v : JSON.stringify(v);
     },
     unconvertValue: function (v) {
-        if ( v.indexOf("{") === 0 || v.indexOf("[") === 0 ){
+        if ( v.indexOf("{") === 0 || v.indexOf("[") === 0 ) {
             var v = JSON.parse(v);
+            return v;
+        } else {
+            return null;
         }
-        return v;
     },
     set: function (type, k, v) {
         var v = this.convertValue(v);
         store.types[type].setItem(k,v); 
     },
     get: function (type, k) {
-        var v = typeof k !== "number" ? store.types[type].getItem(k) : store.types[type].key(k);
+        var v = typeof k !== "number" ? store.types[type].getItem(k) : store.types[type].getItem(store.types[type].key(k));
         return  this.unconvertValue(v);
     },
-    del: function (type, k){
+    del: function (type, k) {
         store.types[type].removeItem(k);       
     },
-    clr: function (type){
+    clr: function (type) {
         store.types[type].clear();
     }
 };
