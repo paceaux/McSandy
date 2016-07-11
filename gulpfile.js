300764d97ab833efa3afd7f76d4b7c531e58bae0
Frank Taylor <frank@tahzoo.com>
2016-07-11 14:52:44 -0500

    sets task for building components

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,22 @@ var		gulp = require('gulp'),
 		stylus = require('gulp-stylus'),
 		nib = require('nib'),
 		concat = require('gulp-concat-util'),
-		uglify = require('gulp-uglify');
+		uglify = require('gulp-uglify'),
+		inline = require('gulp-inline');
 
 var config = {
 	src: {
 		css: 'src/css/*.styl',
 		js: 'src/js/*.js',
-		html: 'src/html'
+		shell: 'src/pre-mcsandy.html',
+		html: 'src/html/*.html'
 	},
 	dist: {
 		css: 'mcsandy.min.css',
-		js: 'mcsandy.min.js'
+		js: 'mcsandy.min.js',
+		html: 'components.html'
 	}
-}
+};
 
 gulp.task('stylus', function () {
 	return gulp.src(config.src.css)
@@ -45,4 +48,14 @@ gulp.task('js', function () {
 		.pipe(concat.header('/* MCSANDY: THE OFFLINE HTML5 SANDBOX */\n' + 'var store, mcsandyAppData, mcsandy, mcsandyPrefs, mcsandyUI;\n'))
 		.pipe(concat.footer('\n' + 'mcsandyUI.init();\n' + 'mcsandy.init();\n' + 'mcsandyPrefs.init();\n'))
 		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('componentbuild', function () {
+	return gulp.src(['src/html/header.html', 'src/html/main.html', 'src/html/footer.html', 'src/html/modal.html'])
+	.pipe(
+		concat(
+			config.dist.html
+		)
+	)
+	.pipe(gulp.dest('dist/'));
 });
