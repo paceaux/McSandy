4089d226335d61d01c4cd7d0fc3103a1854a9abb
paceaux <frank.m.taylor08@gmail.com>
2019-04-18 12:32:48 -0500

    sets store to appease linter

--- src/js/0-store.js
+++ src/js/0-store.js
@@ -1,24 +1,26 @@
-store = {
+
+const { localStorage, sessionStorage } = window;
+const store = {
     types: [localStorage, sessionStorage],
-    convertValue(v) {
-        return typeof v !== 'object' ? v : JSON.stringify(v);
+    convertValue(val) {
+        return typeof val !== 'object' ? val : JSON.stringify(val);
     },
-    unconvertValue(v) {
-        if (v !== null) {
-            if (v.indexOf('{') === 0 || v.indexOf('[') === 0) {
-                v = JSON.parse(v);
-                return v;
-            }
-            return null;
+    unconvertValue(val) {
+        let result;
+
+        if (val && (val.indexOf('{') === 0 || val.indexOf('[') === 0)) {
+            result = JSON.parse(result);
         }
+
+        return result;
     },
-    set(type, k, v) {
-        v = this.convertValue(v);
-        store.types[type].setItem(k, v);
+    set(type, key, val) {
+        const value = this.convertValue(val);
+        store.types[type].setItem(key, value);
     },
-    get(type, k) {
-        v = typeof k !== 'number' ? store.types[type].getItem(k) : store.types[type].getItem(store.types[type].key(k));
-        return this.unconvertValue(v);
+    get(type, key) {
+        const value = typeof key !== 'number' ? store.types[type].getItem(key) : store.types[type].getItem(store.types[type].key(key));
+        return this.unconvertValue(value);
     },
     del(type, k) {
         store.types[type].removeItem(k);
