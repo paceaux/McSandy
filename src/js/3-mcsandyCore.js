a7ce52b9ac1ef7b7d46ce6f01fe5fcbb6bf39ae9
paceaux <frank.m.taylor08@gmail.com>
2019-05-30 13:14:38 -0500

    creates preview class

--- src/js/3-mcsandyCore.js
+++ src/js/3-mcsandyCore.js
@@ -119,6 +119,50 @@ const PreviewTemplates = {
         return `${externalSavedJS}${externalUnsavedJS}${userJS}</body>`;
     },
 };
+
+class Preview {
+    constructor(parts = [], type = 'text/html') {
+        // eslint-disable-next-line no-underscore-dangle
+        this.__parts = parts;
+        this.reset = 'html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{border:0;font-size:100%;font:inherit;vertical-align:baseline;margin:0;padding:0}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:none}table{border-collapse:collapse;border-spacing:0}';
+        this.type = type;
+    }
+
+    get parts() {
+        // eslint-disable-next-line no-underscore-dangle
+        return this.__parts;
+    }
+
+    set parts(newParts) {
+        if (!newParts) return;
+        let hasNewParts = false;
+
+        for (let idx = 0; idx < newParts.length; idx += 1) {
+            if (this.parts[idx] !== newParts[idx]) {
+                hasNewParts = true;
+                break;
+            }
+        }
+
+        if (hasNewParts) {
+            // eslint-disable-next-line no-underscore-dangle
+            this.__parts = newParts;
+        }
+    }
+
+    get blobType() {
+        return `${this.type};charset=utf-8`;
+    }
+
+    get blob() {
+        return new Blob(this.parts, { type: this.blobType });
+    }
+
+    get url() {
+        return window.URL.createObjectURL(this.blob);
+    }
+
+}
 // eslint-disable-next-line no-unused-vars
 const mcsandy = {
     init(data) {
@@ -131,6 +175,8 @@ const mcsandy = {
         Object.keys(this.functions).forEach(funcName => {
             this.functions[funcName] = this.functions[funcName].bind(this);
         });
+
+        this.preview = new Preview();
         this.bindUiEvents();
         this.functions.createProjectSelect();
         if (navigator.onLine) {
@@ -224,13 +270,13 @@ const mcsandy = {
 
             return bodyClose;
         },
-        wrapBlobParts() {
+        getContentFromUI() {
             const head = this.helpers.constructHead();
             const bodyOpen = this.helpers.constructBodyOpen();
             const bodyClose = this.helpers.constructBodyClose();
-            const blobKit = [head, bodyOpen, bodyClose];
+            const contentParts = [head, bodyOpen, bodyClose];
 
-            return blobKit;
+            return contentParts;
         },
         createProjectObj(projectName, rawParts, blobArray, assets) {
             return {
@@ -343,9 +389,10 @@ const mcsandy = {
             const { iframe } = this.data.targets;
             const parts = loadedParts !== undefined
                 ? loadedParts.blobArray
-                : this.helpers.wrapBlobParts();
-            const result = this.helpers.buildBlob(parts);
-            iframe.src = window.URL.createObjectURL(result);
+                : this.helpers.getContentFromUI();
+            this.preview.parts = parts;
+
+            iframe.src = this.preview.url;
         },
         delContent(e) {
             e.preventDefault();
@@ -369,7 +416,7 @@ const mcsandy = {
                 ctrls.js.value,
                 this.blobData.externalJS,
             );
-            const blobArray = this.helpers.wrapBlobParts();
+            const blobArray = this.helpers.getContentFromUI();
             const projectName = this.data.ctrls.projectName.value;
             const externalAssets = this.helpers.createExternalAssetsObj();
             const project = this.helpers.createProjectObj(
@@ -390,7 +437,7 @@ const mcsandy = {
             // type is presumed to be either html, css, or js
             const parts = downloadObj !== undefined
                 ? downloadObj.blobArray
-                : this.helpers.wrapBlobParts();
+                : this.helpers.getContentFromUI();
             const mimeType = type !== 'javascript'
                 ? `text/${type}`
                 : 'application/javascript';
