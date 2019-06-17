558d89b40424c2965a5bd4e582ccf733ec16a1a5
paceaux <frank.m.taylor08@gmail.com>
2019-06-17 10:26:26 -0500

    removes default information

--- src/js/3-mcsandyCore.js
+++ src/js/3-mcsandyCore.js
@@ -20,11 +20,6 @@ const mcsandy = {
         this.editorState = editorState;
         this.bindUiEvents();
     },
-    blobData: {
-        reset: 'html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{border:0;font-size:100%;font:inherit;vertical-align:baseline;margin:0;padding:0}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:none}table{border-collapse:collapse;border-spacing:0}',
-        jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
-        externalJS: [],
-    },
     helpers: {
         prepareHTML(html) {
             return html;
@@ -53,11 +48,11 @@ const mcsandy = {
             return rawParts;
         },
         constructHead() {
-            const { editorState } = this;
+            const { editorState, data } = this;
             const { css, externalCss, jsLibraries } = editorState;
 
             const head = this.SandboxTemplates.Head(
-                this.blobData.reset,
+                data.sandboxDefaults.reset,
                 externalCss,
                 css,
                 jsLibraries,
